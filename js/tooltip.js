b579fd34c4a06c33ee2878c2d52f37b4f902d66e
Dima <indjq@yandex.ru>
2020-06-25 00:17:28 +0400

    стили и tooltip

--- js/tooltip.js
+++ js/tooltip.js
@@ -1,29 +1,55 @@
 function setToolTip(e) {
+
     let et = e.target;
-    e.stopPropagation()
-    if (setToolTip.target != et) {
-        if (setToolTip.tooltip) {
-            setToolTip.tooltip.remove()
-            if (setToolTip.target) setToolTip.target = "";
-        }
+    // e.stopPropagation()
+    if (!setToolTip.tooltip && setToolTip.target != et) {
+
         if (et.className == "week-day-info") {
             setToolTip.target = et;
             let coords = setToolTip.target.getBoundingClientRect()
-            console.log(coords);
-            let tooltip = document.createElement("div");
-            tooltip.classList.add("tooltip")
-            tooltip.style.left = coords.x + coords.width + 10 + "px";
-            tooltip.style.top = coords.y - 10 + "px";
-            document.body.append(tooltip)
-            setToolTip.tooltip = tooltip;
-            document.addEventListener('click', function (event) {
-                var e = setToolTip.tooltip;
-                if (!e.contains(event.target)) e.remove()
-            });
+            setToolTip.tooltip = createToolTip(coords);
         }
 
 
     }
+}
+
+function delToolTip() {
+    var e = setToolTip.tooltip;
+    e.remove();
+    setToolTip.target = "";
+    setToolTip.tooltip = "";
+}
+
+function createToolTip(coords) {
+    function crInput(n, t) {
+        n.forEach(i => {
+            let el = document.createElement("input");
+            el.placeholder = i;
+            t.append(el)
+        });
+    }
+
+    function crBtns(n, t) {
+        n.forEach(i => {
+            let el = document.createElement("button");
+            if (i == "Готово") el.onclick = delToolTip;
+            el.innerHTML = i;
+            t.append(el)
+        });
+    }
+    let tooltip = document.createElement("div");
+    let text = document.createElement("textarea");
+    text.placeholder = "Описание";
+    tooltip.classList.add("tooltip")
+    tooltip.style.left = coords.x + coords.width + 10 + "px";
+    tooltip.style.top = coords.y - 10 + "px";
+    // tooltip.addEventListener('click', delToolTip);
+    crInput([1, 2, 3], tooltip)
+    document.body.append(tooltip)
+    tooltip.append(text)
+    crBtns(["Готово", "Удалить"], tooltip)
+    return tooltip
 }
 setToolTip.target = "";
 setToolTip.tooltip = "";
