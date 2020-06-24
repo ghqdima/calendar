62b51f0384a72e92bbb1c1f0ec79b46d62d7500b
Dima <indjq@yandex.ru>
2020-06-24 22:07:57 +0400

    статические переменные

--- js/tooltip.js
+++ js/tooltip.js
@@ -1,24 +1,19 @@
-const toolTipOb = {
-    target: "",
-    tooltip: ""
-}
-
 function setToolTip(e) {
     let et = e.target;
     e.stopPropagation()
-    if (toolTipOb.target != et) {
-        if (toolTipOb.tooltip) {
-            toolTipOb.tooltip.remove()
-            if (toolTipOb.target) toolTipOb.target = "";
+    if (setToolTip.target != et) {
+        if (setToolTip.tooltip) {
+            setToolTip.tooltip.remove()
+            if (setToolTip.target) setToolTip.target = "";
         }
         if (et.className == "week-day-info") {
-            toolTipOb.target = et;
+            setToolTip.target = et;
             let tooltip = document.createElement("div");
             tooltip.classList.add("tooltip")
             document.body.append(tooltip)
-            toolTipOb.tooltip = tooltip;
+            setToolTip.tooltip = tooltip;
             document.addEventListener('click', function (event) {
-                var e = toolTipOb.tooltip;
+                var e = setToolTip.tooltip;
                 if (!e.contains(event.target)) e.remove()
             });
         }
@@ -26,3 +21,5 @@ function setToolTip(e) {
 
     }
 }
+setToolTip.target = "";
+setToolTip.tooltip = "";
