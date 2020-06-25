f05f0c0a97ae51cdb0a7d15e00cf2531b88ad120
Dima <indjq@yandex.ru>
2020-06-25 21:14:43 +0400

    координаты tooltip

--- js/tooltip.js
+++ js/tooltip.js
@@ -7,7 +7,13 @@ function setToolTip(e) {
         if (et.className == "week-day-info") {
             setToolTip.target = et;
             let coords = setToolTip.target.getBoundingClientRect()
-            setToolTip.tooltip = createToolTip(coords);
+            setToolTip.tooltip = createToolTip();
+            if ((coords.bottom + pageYOffset) < 900)
+                coordsToolTip.leftTop(setToolTip.tooltip, coords)
+            else
+                coordsToolTip.leftBottom(setToolTip.tooltip, coords);
+            if ((coords.right + pageXOffset) > 900)
+                coordsToolTip.rTop(setToolTip.tooltip, coords)
         }
 
 
@@ -21,7 +27,7 @@ function delToolTip() {
     setToolTip.tooltip = "";
 }
 
-function createToolTip(coords) {
+function createToolTip() {
     function crInput(n, t) {
         n.forEach(i => {
             let el = document.createElement("input");
@@ -41,15 +47,32 @@ function createToolTip(coords) {
     let tooltip = document.createElement("div");
     let text = document.createElement("textarea");
     text.placeholder = "Описание";
-    tooltip.classList.add("tooltip")
-    tooltip.style.left = coords.x + coords.width + 10 + "px";
-    tooltip.style.top = coords.y - 10 + "px";
-    // tooltip.addEventListener('click', delToolTip);
+
     crInput([1, 2, 3], tooltip)
-    document.body.append(tooltip)
     tooltip.append(text)
     crBtns(["Готово", "Удалить"], tooltip)
+    tooltip.classList.add("tooltip")
+    document.body.append(tooltip)
     return tooltip
 }
+let coordsToolTip = {
+    scrollHeight: Math.max(
+        document.body.scrollHeight, document.documentElement.scrollHeight,
+        document.body.offsetHeight, document.documentElement.offsetHeight,
+        document.body.clientHeight, document.documentElement.clientHeight
+    ),
+    leftTop(el, coords) {
+        el.style.left = coords.x + coords.width + 20 + "px";
+        el.style.top = coords.y - 40 + pageYOffset + "px";
+    },
+    leftBottom(el, coords) {
+        el.style.left = coords.x + coords.width + 20 + "px";
+        el.style.top = coords.y - 170 + pageYOffset + "px";
+    },
+    rTop(el, coords) {
+        el.style.left = coords.x - 370 + "px";
+    }
+}
+
 setToolTip.target = "";
 setToolTip.tooltip = "";
